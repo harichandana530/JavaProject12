091eacc96de65883158a9ced61b9bb63ffdf5b82
Sushmita2205 <sushmitaghatge22@gmail.com>
2020-01-08 17:58:30 +0530

    COMMITTED

--- GitTesting/src/test/Main.java
+++ GitTesting/src/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Working with git");
+		System.out.println("HELLO FRIENDS.....");
 	}
 
 }
